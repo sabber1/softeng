4e457c91ed85694060f05c14a7cfa455f5d32405
sabber1 <szkercza@gmail.com>
2022-11-16 12:49:51 +0100

    Add project files.

---
+++ 8het_S08GKL/Controllers/JokeController.cs
@@ -0,0 +1,65 @@
+﻿using _8het_S08GKL.JokeModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _8het_S08GKL.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        // GET: api/<JokeController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return Ok(context.Jokes.ToList());
+        }
+
+        // GET api/<JokeController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var vicc = (from x in context.Jokes
+                        where x.JokeSk == id
+                        select x).FirstOrDefault();
+            if (vicc == null)
+            {
+                return NotFound($"Nincs #{id} azonosítóval vicc");
+            }
+            else
+            {
+                return Ok(vicc);
+            }
+        }
+
+        // POST api/<JokeController>
+        [HttpPost]
+        public void Post([FromBody] Joke vicc)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(vicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JokeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+    }
+}
