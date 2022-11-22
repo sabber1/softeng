737d1de020e928e9793afc15265100d690719334
sabber1 <szkercza@gmail.com>
2022-11-23 00:11:51 +0100

    10. heti gyak

---
+++ 8het_S08GKL/wwwroot/geeks.js
@@ -0,0 +1,31 @@
+﻿var viccek;
+
+window.onload = () => {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data))
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés");
+    console.log(d);
+    viccek = d;
+
+    for (var i = 0; i < viccek.length; i++) {
+        var viccDiv = document.createElement("div");
+        viccDiv.className = "vicc";
+
+        var viccQuestion = document.createElement("p");
+        viccQuestion.className = "q"
+        viccQuestion.innerHTML = viccek[i].question;
+
+        var viccAnswer = document.createElement("p");
+        viccAnswer.className = "a"
+        viccAnswer.innerHTML = viccek[i].answer;
+
+        viccDiv.appendChild(viccQuestion);
+        viccDiv.appendChild(viccAnswer);
+
+        document.getElementById("jokes").appendChild(viccDiv);
+    }
+}
---
+++ 8het_S08GKL/wwwroot/jsgyak.js
@@ -0,0 +1,29 @@
+﻿window.onload = () => {
+    doThepascal();
+}
+function faktoriális(num) {
+    if (num < 0) {
+        return -1
+    }
+    else if (num == 0) {
+        return 1
+    }
+    else {
+        return (num * faktoriális(num - 1));
+    }
+}
+
+function doThepascal() {
+    for (var sor = 0; sor < 10; sor++) {
+        var sorDiv = document.createElement("div");
+        sorDiv.className = "sor";
+        document.getElementById("pascal").appendChild(sorDiv);
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var elemDiv = document.createElement("div")
+            elemDiv.className = "elem"
+            elemDiv.innerHTML = faktoriális(sor) / (faktoriális(oszlop) * faktoriális(sor - oszlop));
+            sorDiv.appendChild(elemDiv)
+        }
+    }
+}
