81c95f5911dbdbd1f81dc3078c582a5dac075fce
sabber1 <szkercza@gmail.com>
2022-12-01 11:58:41 +0100

    Basic Boat

---
+++ 8het_S08GKL/Controllers/BoatController.cs
@@ -0,0 +1,18 @@
+﻿using _8het_S08GKL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _8het_S08GKL.Controllers
+{
+    public class BoatController : Controller
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajoContext context = new HajoContext();
+            var kérédesek = from x in context.Questions select x.Question1;
+
+            return Ok(kérédesek);
+        }
+    }
+}
---
+++ 8het_S08GKL/Controllers/TesztController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _8het_S08GKL.Controllers
+{
+    [ApiController]
+    public class TesztController : Controller
+    {
+        [HttpGet]
+        [Route("corvinus/szerveridő")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain
+                Content = pontosIdő
+            };
+        }
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            if (string.IsNullOrEmpty(szoveg))
+            {
+                return BadRequest("Nem megfelelő a bemenő adat");
+            }
+
+            return Ok(szoveg.ToUpper());
+        }
+    }
+}
